a4af24812616e4985cfc107fde6bfb83d3ab8e4b
Eva-MarieH <hamel.eva-marie@hotmail.fr>
2025-02-05 18:31:52 +0100

    middleware pile

--- server.js
+++ server.js
@@ -17,6 +17,19 @@ const server = https.createServer(options, app).listen(3212, () => {
     console.log('HTTPS => listening on 3212');
 });
 
+app.use('/produit/:nom/:stock',(req, res, next) => { // 1e fonction middleware dans la pile
+    console.log('Appelé à chaque requête entrante : première fois');
+    var nom = req.params.nom;
+    var stock = req.params.stock - 1;
+    next(); // Appelle la fonction middleware suivante
+});
+app.use('/produit/:nom/:stock',(req, res, next) => {// 2e fonction middleware dans la pile
+    console.log('Appelé à chaque requête entrante : deuxieme fois');
+    var nom = req.params.nom;
+    var stock = req.params.stock - 1;
+    next(); // Appelle la fonction middleware suivante
+});
+
 app.get('/produit/:nom/:stock', (req, res) => {
     console.log('Appel sur requête entrante /produit');
     var nom = req.params.nom;
