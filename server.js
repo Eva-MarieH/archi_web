f577283af01006c4b499ba7b151e80d4e4739fa2
Eva-MarieH <hamel.eva-marie@hotmail.fr>
2025-02-05 17:33:30 +0100

    https

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
 const app = express();
 
 app.listen(3211, function() {
     console.log('Server listening on port 3211');
+});
+
+const options = {
+    key: fs.readFileSync('key.pem'), // lecture clé privée et certificat
+    cert: fs.readFileSync('cert.pem')
+};
+// lancement du serveur https avec clé et certificat associé
+const server = https.createServer(options, app).listen(3212, () => {
+    console.log('HTTPS => listening on 3212');
 });
 
 
