dc3b2ecb24970b0f2780de8da86043c8c569b094
Eva-MarieH <hamel.eva-marie@hotmail.fr>
2025-02-11 09:30:19 +0100

    modif server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/produit/:nom/:stock', (req, res) => {
 
 app.get('/', (req, res) => {
     console.log('Appel sur requête entrante /redirect');
-    res.sendFile('/appTest/dist/app-test/browser/index.html');
+    res.sendFile('appTest/dist/app-test/browser/index.html');
     //res.send('Méthode GET'); // termine le cycle
 });
 
