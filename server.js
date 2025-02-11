cf11940dcc17f52cb55b4521c4540d600f8fe524
Eva-MarieH <hamel.eva-marie@hotmail.fr>
2025-02-11 10:04:18 +0100

    correction

--- server.js
+++ server.js
@@ -17,26 +17,6 @@ const server = https.createServer(options, app).listen(3212, () => {
     console.log('HTTPS => listening on 3212');
 });
 
-app.use('/produit/:nom/:stock', (req, res, next) => { // 1e fonction middleware dans la pile
-    console.log('Appelé à chaque requête entrante : première fois');
-    req.stock = req.params.stock - 1;
-    next(); // Appelle la fonction middleware suivante
-});
-app.use('/produit/:nom/:stock', (req, res, next) => {// 2e fonction middleware dans la pile
-    console.log('Appelé à chaque requête entrante : deuxieme fois');
-    req.stock = req.stock - 1;
-    next(); // Appelle la fonction middleware suivante
-});
-
-app.get('/produit/:nom/:stock', (req, res) => {
-    console.log('Appel sur requête entrante /produit');
-    var nom = req.params.nom;
-    var stock = req.stock;
-    console.log('Nom : ' + nom);
-    console.log('Stock : ' + stock);
-    res.json({ 'nom': nom, 'stock': stock });
-});
-
 app.get('/', (req, res) => {
     console.log('Appel sur requête entrante /');
     res.sendFile('/nfs/data01/data/uapv25/uapv2502990/public_html/archi_web/appTest/dist/app-test/browser/index.html');
